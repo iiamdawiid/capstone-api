a0140ee902bfd83f551875d75682506cde85949e
David Jachym <jachym19@gmail.com>
2023-11-10 16:31:36 -0600

    add: save and delete routes for cal calculator

--- app/calorie_calculator/routes.py
+++ app/calorie_calculator/routes.py
@@ -3,6 +3,8 @@ from flask import request
 from flask_jwt_extended import jwt_required, current_user
 from ..models import CalorieCalculator
 
+
+
 @c.post('/save_calories')
 @jwt_required()
 def handle_save_calories():
@@ -19,9 +21,23 @@ def handle_save_calories():
     weight = body.get('weight')
     height = body.get('height')
     age = body.get('age')
+    units = body.get('units')
+    calories = body.get('calories')
+    gain_weight1 = body.get('gain_weight1')
+    gain_weight2 = body.get('gain_weight2')
+    gain_weight3 = body.get('gain_weight3')
+    lose_weight1 = body.get('lose_weight1')
+    lose_weight2 = body.get('lose_weight2')
+    lose_weight3 = body.get('lose_weight3')
     saved_by = body.get('saved_by')
 
-    saved_calories = CalorieCalculator(gender=gender, activity_level=activity_level, weight=weight, height=height, age=age, saved_by=saved_by)
+    saved_calories = CalorieCalculator(
+        gender=gender, activity_level=activity_level, weight=weight, height=height, 
+        age=age, units=units, calories=calories, gain_weight1=gain_weight1, gain_weight2=gain_weight2, 
+        gain_weight3=gain_weight3, lose_weight1=lose_weight1, lose_weight2=lose_weight2, 
+        lose_weight3=lose_weight3, saved_by=saved_by
+        )
+    
     saved_calories.create()
 
     response = {
@@ -29,3 +45,28 @@ def handle_save_calories():
         "calories": saved_calories.to_response()
     }
     return response, 201
+
+
+
+@c.delete('/delete_calories/<save_id>')
+@jwt_required()
+def handle_calorie_delete(save_id):
+    saved_calories = CalorieCalculator.query.filter_by(id=save_id).one_or_none()
+    if saved_calories is None:
+        response = {
+            "message": "the saved calories do not exist"
+        }
+        return response, 404
+    
+    if saved_calories.saved_by != current_user.id:
+        response = {
+            "message": "can not delete another user's saved calories"
+        }
+        return response, 401
+    
+    saved_calories.delete()
+
+    response = {
+        "message": "saved calories successfully deleted"
+    }
+    return response, 200
--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class User(db.Model):
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     calorie_calculator = db.relationship('CalorieCalculator', backref='user')
+    onerepmax = db.relationship('OneRepMax', backref='user')
     
     def __init__(self, email, username, password):
         self.id = str(uuid4())
@@ -52,19 +53,37 @@ class CalorieCalculator(db.Model):
     id = db.Column(db.String(64), primary_key=True)
     gender = db.Column(db.String(6), nullable=False)
     activity_level = db.Column(db.String(30), nullable=False)
-    weight = db.Column(db.String(10), nullable=False)
-    height = db.Column(db.String(10), nullable=False)
+    weight = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
     age = db.Column(db.Integer, nullable=False)
+    units = db.Column(db.String(10), nullable=False)
+    calories = db.Column(db.Integer, nullable=False)
+
+    gain_weight1 = db.Column(db.Integer, nullable=False)
+    gain_weight2 = db.Column(db.Integer, nullable=False)
+    gain_weight3 = db.Column(db.Integer, nullable=False)
+    lose_weight1 = db.Column(db.Integer, nullable=False)
+    lose_weight2 = db.Column(db.Integer, nullable=False)
+    lose_weight3 = db.Column(db.Integer, nullable=False)
+
     saved_at = db.Column(db.DateTime, default = datetime.utcnow)
     saved_by = db.Column(db.String(64), db.ForeignKey('user.id'), nullable=False)
 
-    def __init__(self, gender, activity_level, weight, height, age, saved_by):
+    def __init__(self, gender, activity_level, weight, height, age, units, calories, gain_weight1, gain_weight2, gain_weight3, lose_weight1, lose_weight2, lose_weight3, saved_by):
         self.id = str(uuid4())
         self.gender = gender
         self.activity_level = activity_level
         self.weight = weight
         self.height = height
         self.age = age
+        self.units = units
+        self.calories = calories
+        self.gain_weight1 = gain_weight1
+        self.gain_weight2 = gain_weight2
+        self.gain_weight3 = gain_weight3
+        self.lose_weight1 = lose_weight1
+        self.lose_weight2 = lose_weight2
+        self.lose_weight3 = lose_weight3
         self.saved_by = saved_by
 
     def create(self):
@@ -83,6 +102,84 @@ class CalorieCalculator(db.Model):
             "weight": self.weight,
             "height": self.height,
             "age": self.age,
+            "units": self.units,
+            "calories": self.calories,
+            "gain_weight1": self.gain_weight1,
+            "gain_weight2": self.gain_weight2,
+            "gain_weight3": self.gain_weight3,
+            "lose_weight1": self.lose_weight1,
+            "lose_weight2": self.lose_weight2,
+            "lose_weight3": self.lose_weight3,
+            "saved_at": self.saved_at,
+            "saved_by": self.saved_by
+        }
+    
+class OneRepMax(db.Model):
+    id = db.Column(db.String(64), primary_key=True)
+    type_of_lift = db.Column(db.String(15), nullable=False)
+    weight = db.Column(db.Integer, nullable=False)
+    reps = db.Column(db.Integer, nullable=False)
+    units = db.Column(db.String(10), nullable=False)
+    one_rep_max = db.Column(db.String(50), nullable=False)
+
+    percentage1 = db.Column(db.Integer, nullable=False)
+    percentage2 = db.Column(db.Integer, nullable=False)
+    percentage3 = db.Column(db.Integer, nullable=False)
+    percentage4 = db.Column(db.Integer, nullable=False)
+    percentage5 = db.Column(db.Integer, nullable=False)
+    percentage6 = db.Column(db.Integer, nullable=False)
+    percentage7 = db.Column(db.Integer, nullable=False)
+    percentage8 = db.Column(db.Integer, nullable=False)
+
+    saved_at = db.Column(db.DateTime, nullable=False)
+    saved_by = db.Column(db.String(64), db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(
+            self, type_of_lift, weight, reps, 
+            units, one_rep_max, percentage1, percentage2, 
+            percentage3, percentage4, percentage5, percentage6, 
+            percentage7, percentage8, saved_by):
+        
+        self.id = str(uuid4())
+        self.type_of_lift = type_of_lift
+        self.weight = weight
+        self.reps = reps
+        self.units = units
+        self.one_rep_max = one_rep_max
+        self.percentage1 = percentage1
+        self.percentage2 = percentage2
+        self.percentage3 = percentage3
+        self.percentage4 = percentage4
+        self.percentage5 = percentage5
+        self.percentage6 = percentage6
+        self.percentage7 = percentage7
+        self.percentage8 = percentage8
+        self.saved_by = saved_by
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def to_response(self):
+        return {
+            "id": self.id,
+            "type_of_lift": self.type_of_lift,
+            "weight": self.weight,
+            "reps": self.reps,
+            "units": self.units,
+            "one_rep_max": self.one_rep_max,
+            "percentage1": self.percentage1,
+            "percentage2": self.percentage2,
+            "percentage3": self.percentage3,
+            "percentage4": self.percentage4,
+            "percentage5": self.percentage5,
+            "percentage6": self.percentage6,
+            "percentage7": self.percentage7,
+            "percentage8": self.percentage8,
             "saved_at": self.saved_at,
             "saved_by": self.saved_by
         }
